e9557f8dfc12f7519d985cc9e0883194987c0976
joe <walktheworld@me.com>
2021-04-07 16:41:11 -0600

    commit tree 494970aca0fd44118d2b0c7b07ba35465b9e86b5

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+let num1 = 2;
+let num2 = 31;
+let multiply = num1*num2;
 
-// Write your code here
+
+let random = Math.floor(Math.random() * 50) + 1;
+
+let num3= 20;
+let num4 = 5;
+let mod = num3/num4;
+
+let max = Math.max(5, 10, 15, 20)
